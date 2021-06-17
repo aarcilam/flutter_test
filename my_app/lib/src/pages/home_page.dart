09a3deb5d6b1542d78b877b6efe0f84426498e80
Andres Arcila <andresarcila2@me.com>
2021-06-16 23:11:35 -0500

    layout del home con boton flotante

--- my_app/lib/src/pages/home_page.dart
+++ my_app/lib/src/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   // creo un nuevo widget sin estado 
+  final estiloTexto = new TextStyle(fontSize: 20);
   // sobre escribo el metodo build del widget 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +12,19 @@ class HomePage extends StatelessWidget {
         title: Text('logo'),
       ),
       body: Center(
-        child: Text('hola mundo'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Numero de clicks',style: estiloTexto),
+            Text('texto 2')
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          print('hola');
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
